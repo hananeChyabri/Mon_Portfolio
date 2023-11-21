c3734d9f7a8801eceaec7d34ca2bafa9b55d0a5c
unknown <chyabri.hanane@gmail.com>
2023-11-21 10:58:08 +0100

    3 thems colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,30 @@ let mixerPortfolio = mixitup('.work__container', {
         duration: 300
     }
 });
+
+/*POPUP WORK PORTFOLIO*/
+
+document.addEventListener("click",(event)=>{
+    if(event.target.classList.contains("work__button"))
+    {
+        OpenClosePortfolio();
+        portfolioItemDetails(event.target.parentElement);
+    }
+});
+
+document.querySelector(".portfolio__popup-close").addEventListener("click",OpenClosePortfolio);
+//fonction pour ouvrir/ fermer le popup de chaque work du portfolio
+function OpenClosePortfolio(){
+
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+
+}
+//changer le detail de chaque item du pop up selon le work choisi
+function portfolioItemDetails(parentElement){
+    console.log(parentElement.querySelector(".img__work"));
+
+    document.querySelector(".pp__img img").src = parentElement.querySelector(".img__work").src;
+   // document.querySelector(".pp__img img").src = parentElement.querySelector(".img__work").src;
+    document.querySelector(".portfolio__popup-subtitle").innerHTML = parentElement.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = parentElement.querySelector(".portfolio__item-details").innerHTML;
+}
