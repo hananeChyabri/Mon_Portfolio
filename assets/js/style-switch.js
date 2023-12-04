169d9548a95969a2e3750a73ea245faa1d6d277e
unknown <chyabri.hanane@gmail.com>
2023-12-04 18:02:56 +0100

    titre image soft skills

--- assets/js/style-switch.js
+++ assets/js/style-switch.js
@@ -54,6 +54,21 @@ switchLnaguageIcon.addEventListener('click', (event) => {
             document.querySelector('.home__title').innerText = data.home__title;
             document.querySelector('.home__subtitle').innerText = data.home__subtitle;
             document.querySelector('.home__description').innerText = data.home__description;
+            document.querySelector('.about_info .section__title').innerText = data.about__title;
+           // document.querySelector('.about__description').innerText = data.about__description;
+           // document.querySelector('.about__description .textDancing').innerText = data.about__image_title;
+            document.querySelector('.qualification_title').innerText = data.qualification_title;
+           
+            document.querySelectorAll('.timeline__item .timeline__text')[0].innerText = data.timeline__text1;
+      
+
+
+
+            // "about__title" : "About Me",
+            // "about__description": "Passionate about web development, my educational background and professional journey have equipped me with robust technical skills in web development. ",
+            // "about__image_title": "To discover my soft skills, click on the dots within the image.",
+            // "qualification_title": "Education",
+            // "timeline__text1": "Web Applicatio devlopper"
 
             
             
